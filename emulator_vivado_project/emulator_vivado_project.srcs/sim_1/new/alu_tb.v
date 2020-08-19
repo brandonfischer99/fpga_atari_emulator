0d756e74878e19d3ca6045418a331ccf7a51ada7
brandon <brandonfischer99@gmail.com>
2020-08-19 17:46:42 -0400

    Added checkAdd task to testbench to check the outputs for any addition OPs in the ALU. Everything looks correct

--- emulator_vivado_project/emulator_vivado_project.srcs/sim_1/new/alu_tb.v
+++ emulator_vivado_project/emulator_vivado_project.srcs/sim_1/new/alu_tb.v
@@ -40,23 +40,42 @@ alu test_unit(
     out,CarryOut,ZeroFlag,Overflow,NegativeFlag
 );
 
-task checkAdd(
-    input [7:0] A, input [7:0] B
-    );
+task checkAdd;
+    input [7:0] testA;
+    input [7:0] testB;
+    static reg [9:0] tmp = {1'b0,testA} + {1'b0,testB};
+    
     static bit [7:0] OPS [0:7] = {8'h69, 8'h65, 8'h75, 8'h6d, 8'h7d, 8'h79, 8'h61, 8'h71};
     begin
         $display("Testing Add\n");
-        
+        for(integer i = 0; i < 8; i++)
+        begin
+            $display("i = %d", i);
+            OP = OPS[i];
+            A = testA;
+            B = testB;
+            #5
+            assert( out == (testA + testB)) else $fatal(1, "Bad add\n");
+            assert( CarryOut == tmp[8]) else $fatal(1, "Bad Carryout\n");
+            assert( ZeroFlag == (tmp[7:0] == 8'h00)) else $fatal(1, "Bad Zero flag\n");
+            assert( Overflow == (CarryOut ^ tmp[7])) else $fatal(1, "Bad Overflow Flag\n");
+            assert( NegativeFlag == tmp[7]) else $fatal(1, "Bad Negative Flag\n");
+        end
     end
 endtask
 
 initial begin
-    A = 8'h0;
-    B = 8'h0;
-    CarryIn = 0;
-    #100;//hold reset
+
+    checkAdd(8'h1,8'h1);
     
+    checkAdd(8'h0, 8'h0);
     
+    checkAdd(8'hff,8'hff);
+    
+    checkAdd(8'h80, 8'h80);
+    
+    $display("@@@Passed\n");
+    $finish;
     
 end
 
